14d67c680e7fd7323cff034d52ceff8073bca2d8
root <sylyac2000@gmail.com>
2022-04-25 09:41:23 -0700

    Write a function that produces output according to a format.

---
+++ char.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_char - returns char as string
+ * @list: char
+ * Return: String verion of char
+ */
+char *print_char(va_list list)
+{
+	char *s;
+	char c;
+
+	c = va_arg(list, int);
+
+	if (c == 0)
+		c = '\0';
+
+	s = malloc(sizeof(char) * 2);
+	if (s == NULL)
+		return (NULL);
+	s[0] = c;
+	s[1] = '\0';
+
+	return (s);
+}
